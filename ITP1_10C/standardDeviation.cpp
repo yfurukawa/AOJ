400ad324ab04364fa6796795f64f134becd0cbde
Yoshihiro Furukawa <yoshihiro.furukawa@nifty.com>
2015-05-30 22:00:17 +0900

    Fixed bug

--- ITP1_10C/standardDeviation.cpp
+++ ITP1_10C/standardDeviation.cpp
@@ -7,7 +7,7 @@ using namespace std;
 double calculateVariance(const int& numberOfStudent, const int& sum, const double& avarage, const int* const score);
  
 int main() {
-  int numberOfStudent(0), score[1000], sum(0);
+  double numberOfStudent(0.0), score[1000], sum(0.0);
   double avarage(0.0), variance(0.0);
 
   while(1) {
@@ -21,21 +21,16 @@ int main() {
       sum += score[count];
     }
     avarage = sum / numberOfStudent;
-    
-    variance = calculateVariance(numberOfStudent, sum, avarage, score);
+ 
+    double tmp(0.0); 
+    for(int i = 0; i < numberOfStudent; ++i) {
+      tmp += pow(((double)score[i] - avarage), 2.0);
+    }
+    variance = tmp / numberOfStudent;
 
     cout << fixed << setprecision(8) << sqrt(variance) << endl;
   }
 
   return 0;
-}
-
-double calculateVariance(const int& numberOfStudent, const int& sum, const double& avarage, const int* const score) {
-  double tmp(0.0);
-  for(int i = 0; i < numberOfStudent; ++i) {
-    tmp += pow((*(score + i) - avarage), 2.0);
-  }
-
-  return tmp / numberOfStudent;
 }
 
